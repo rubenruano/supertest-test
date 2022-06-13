709cc302fdbde7cea56f0bd53ff0ad05a5cf9e68
Ruben Ruano <ruben.ruano@devgurus.io>
2022-06-13 12:36:21 -0300

    commit 11

--- src/flow/calendar.js
+++ src/flow/calendar.js
@@ -2,12 +2,25 @@ import getHolidays from "../modules/calendar/getHolidays";
 
 export default () => {
   describe("Calendar", () => {
-    test("Get holidays", async () => {
+    test("Get all holidays", async () => {
       const response = await getHolidays(200, { country: "AR", year: 2022 });
       const json = JSON.parse(response.response.body);
 
       const holidays = json.response.holidays;
-      expect(holidays.length).toBeGreaterThan(0);
+      expect(holidays.length).toBeGreaterThan(10);
+      expect(holidays[0].date.iso).toBe("2022-01-01");
+    });
+
+    test("Get holidays in a month", async () => {
+      const response = await getHolidays(200, {
+        country: "AR",
+        year: 2022,
+        month: 1
+      });
+      const json = JSON.parse(response.response.body);
+
+      const holidays = json.response.holidays;
+      expect(holidays.length).toBe(1);
       expect(holidays[0].date.iso).toBe("2022-01-01");
     });
   });
--- src/modules/calendar/getHolidays.js
+++ src/modules/calendar/getHolidays.js
@@ -2,13 +2,15 @@ const request = require("supertest");
 
 export default async (expectedStatus, params) => {
   const url = "/holidays";
+  const API_KEY = "455b5a72c3bf47760106a8d34a52c49bf690cb19"; // TODO: this should be in a secret
 
   const res = await request(process.env.CALENDAR_API_ENDPOINT)
     .get(url)
     .query({
-      api_key: "455b5a72c3bf47760106a8d34a52c49bf690cb19",
+      api_key: API_KEY,
       country: params.country,
-      year: params.year
+      year: params.year,
+      month: params.month
     })
     .expect(expectedStatus);
 
